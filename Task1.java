80a58eb76d7048258a6a491e02af92baf6b34844
Tovarish-Kenobov <56266651+Tovarish-Kenobov@users.noreply.github.com>
2023-10-12 09:51:07 +0300

    Upload Task 1 solution

---
+++ Task1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+   public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       int i = scanner.nextInt();
+       if (i > 7) {
+           System.out.println("Привет");
+       }
+   }
+}
